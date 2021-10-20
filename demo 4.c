ad1da0cbc9d02a6b8032dae31cdd96a66a613ae5
YenugaSridhar <92288126+YenugaSridhar@users.noreply.github.com>
2021-10-20 10:58:42 +0530

    Add files via upload

---
+++ demo 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//variable Declaration
+	int age;
+	double cgpa;
+	char initial;
+	
+	// Initialization
+	age = 19;
+	cgpa = 8;
+	initial = 'B';
+		
+		//Output
+		printf("age of the student is %d\n", age);
+		printf("%lf\n", cgpa);
+		printf("%c\n", initial);
+		
+}
