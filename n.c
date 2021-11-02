b51316e95b5825e6a4c2a51450450d294308eccd
YenugaSridhar <92288126+YenugaSridhar@users.noreply.github.com>
2021-11-02 10:52:06 +0530

    Add files via upload

--- n.c
+++ n.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 void main()
 {
-	int a=30,b=15,c,d,e,f,g;
-	c=a+b;
-	d=a-b;
-	e=a*b;
-	f=a/b;
-	g=a%b;
-	printf("sum of %d and %d is %d\n",a,b,c);
-	printf("difference of %d and %d is %d\n",a,b,d);
-	printf("product of %d and %d is %d\n",a,b,e);
-	printf("quotient of %d and %d is %d\n",a,b,f);
-	printf("remainder of %d and %d is %d\n",a,b,g);
-	printf("\nsum of all arithmetic operation is %d",(a+b)+(a-b)+(a*b)+(a/b)+(a%b) );
+	int n=5;
+	int o = n*(n+1)/2;
+	printf("sum of first %d natural numbers is %d\n",n,o);
+	int p = n*(n+1);
+	printf("sum of first %d even natural numbers is %d\n",n,p);
+	int q = n*n;
+	printf("sum of first %d odd natural numbers is %d\n",n,q);
+	
+		
+		
 }
---
+++ n2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int p,t,r,s;
+	p=10000;
+	t=2;
+	r=7.5;
+	s=p*t*r/100;
+	printf("the simple interest when\n");
+	printf("the principal amount is %d\n",p);
+	printf("the time period is %d\n",t);
+	printf("the float is %d\n",r);
+}
